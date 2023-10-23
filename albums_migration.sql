0c8638774f1370c36055ae502fbdc770c6893ba1
Ryan Hampton <hamptonryan88@gmail.com>
2023-10-23 12:23:43 -0500

    made some changes

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 drop table if exists albums;
 
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name VARCHAR(50),
-    release_date DATE,
-    sales FLOAT,
+    id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date int(4) NOT NULL,
+    sales decimal(3,1) NOT NULL,
     genre VARCHAR(50),
     PRIMARY KEY (id)
 );
